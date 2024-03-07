673fac5b32ab1288ffe0c8e98e0b1ea311e6d765
BlueandBlack12 <jamesjrpat@gmail.com>
2024-03-07 19:42:41 +0800

    Add Models and ViewModel

---
+++ Engine/Models/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+     class Player
+    {
+        public string Name { get; set; }
+        public string Class { get; set; }
+       public int HP { get; set; }
+       public int EXP { get; set; }
+        public int Level { get; set; }
+       public int Money { get; set; }
+    }
+}
